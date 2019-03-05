88cbd262af081714ad7cf2dd77335c4c647cb786
mmmarzouki <mmmarzouki@gmail.com>
2019-03-05 12:52:19 +0100

    moved ged folder inside dahsboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExplorerComponent } from './components/GED/explorer/explorer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ExplorerComponent } from './components/dashboard/GED/explorer/explorer.component';
 
 @NgModule({
   declarations: [
