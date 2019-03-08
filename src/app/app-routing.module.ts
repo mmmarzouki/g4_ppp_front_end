eff386b6c3ee070d488c2e8d39d30f580498f192
mmmarzouki <mmmarzouki@gmail.com>
2019-03-08 15:02:12 +0100

    reorganised routes in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import {ExplorerComponent} from './components/dashboard/ged/explorer/explorer.component';
 
+const dashboardRoutes: Routes = [
+    { path: 'ged', component: ExplorerComponent }
+  ];
+
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, children : [ { path: 'ged', component: ExplorerComponent } ] },
+  { path: 'dashboard', component: DashboardComponent, children : dashboardRoutes },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
