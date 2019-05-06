fe4daa5abad8bb5ac634c7d0b14f2c535ae17cad
mmmarzouki <mmmarzouki@gmail.com>
2019-05-06 13:38:16 +0200

    added document service

---
+++ src/app/_services/document.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Project } from '../models/project';
+import { Process } from '../models/process';
+import { Doc } from '../models/doc';
+import { Observable } from 'rxjs';
+import { url } from 'inspector';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+
+@Injectable()
+export class DocumentService {
+
+  private baseUrl = 'http://localhost:3333/doc/'
+
+  constructor(private http: HttpClient) { }
+
+  public viewDocument(project: Project, process: Process, doc: Doc): Observable<any> {
+    const fullUrl = this.baseUrl + project.name +'/' + process.name + '/' + doc.docName ;
+    return this.http.get(fullUrl, httpOptions);
+  }
+
+}
