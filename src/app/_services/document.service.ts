cf287f697d0fabcb78b4f594ea18c5b7fd1bc327
mmmarzouki <mmmarzouki@gmail.com>
2019-05-11 15:22:06 +0200

    added feature to open the project mandate

--- src/app/_services/document.service.ts
+++ src/app/_services/document.service.ts
@@ -2,24 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Doc } from '../models/doc';
 import { Observable } from 'rxjs';
+import { Project } from '../models/project';
 
 
-const httpOptionsFile = {
-  headers: new HttpHeaders({ 'responseType': 'application/pdf' })
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
 };
-
 
 @Injectable()
 export class DocumentService {
 
-  private baseUrl = 'http://localhost:3333/showdoc'
+  private urlDoc = 'http://localhost:3333/showdoc'
+  private urlMandate = 'http://localhost:3333/mandate/'
 
   constructor(private http: HttpClient) { }
 
 
   public viewDocument(doc: Doc): Observable<any> {
     let body = {path: doc.docPath};
-    return this.http.post(this.baseUrl,body, httpOptionsFile);
+    return this.http.post(this.urlDoc,body, httpOptions);
+  }
+
+  public openMandate(project: Project): Observable<any> {
+    const fullUrl = this.urlMandate+project._id;
+    return this.http.get(fullUrl, httpOptions);
   }
 
 }
--- src/app/theme/pages/default/blank/documents/processes/processes.component.ts
+++ src/app/theme/pages/default/blank/documents/processes/processes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Project} from "../../../../../../models/project";
 import {ActivatedRoute} from "@angular/router";
+import { DocumentService } from '../../../../../../_services/document.service';
 
 @Component({
   selector: 'app-processes',
@@ -11,11 +12,15 @@ export class ProcessesComponent implements OnInit {
 
   project: Project;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private documentService: DocumentService) { }
 
 
   ngOnInit() {
     this.project = this.route.snapshot.data.project[0];
   }
 
+  openMandate(){
+    this.documentService.openMandate(this.project).subscribe(res => {
+    });
+  }
 }
