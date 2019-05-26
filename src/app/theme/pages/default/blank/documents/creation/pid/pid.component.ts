31001e72fd74e45ac3440298c948be97a0bfc14e
mmmarzouki <mmmarzouki@gmail.com>
2019-05-27 00:09:05 +0200

    completed pid form

--- src/app/theme/pages/default/blank/documents/creation/pid/pid.component.ts
+++ src/app/theme/pages/default/blank/documents/creation/pid/pid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PidComponent implements OnInit {
 
   background = '';
+  version = '';
+  mainGoal = '';
+  desiredOutcomes = '';
+  interfaces = '';
+  projectApproach = '';
+  acceptanceResponsibilities = '';
+  projectProductDescription = '';
+  businessCase = '';
+  stakeHolderList = '';
+  qualityManagementApproach = '';
+  changeControlApproach = '';
+  riskManagementApproach = '';
+  communicationManagementApproach = '';
+  projectPlan = '';
+  projectManagementTeam = '';
+  projectControls = '';
+  tailoring = '';
   public form: FormGroup;
   submitted= false;
 
@@ -16,7 +33,24 @@ export class PidComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      background: [null, Validators.required]
+      version: [null, Validators.required],
+      background: [null, Validators.required],
+      mainGoal: [null, Validators.required],
+      desiredOutcomes: [null, Validators.required],
+      interfaces: [null, Validators.required],
+      projectApproach: [null, Validators.required],
+      acceptanceResponsibilities: [null, Validators.required],
+      projectProductDescription: [null, Validators.required],
+      businessCase: [null, Validators.required],
+      stakeHolderList: [null, Validators.required],
+      qualityManagementApproach: [null, Validators.required],
+      changeControlApproach: [null, Validators.required],
+      riskManagementApproach: [null, Validators.required],
+      communicationManagementApproach: [null, Validators.required],
+      projectPlan: [null, Validators.required],
+      projectManagementTeam: [null, Validators.required],
+      projectControls: [null, Validators.required],
+      tailoring: [null, Validators.required]
     })
   }
 
