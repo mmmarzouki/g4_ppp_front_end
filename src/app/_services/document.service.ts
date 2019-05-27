a31c28cfc74a08d113d136e5793cc490833851b5
mmmarzouki <mmmarzouki@gmail.com>
2019-05-27 12:39:31 +0200

    added uploadBusinessCase & uploadPid to documentService

--- src/app/_services/document.service.ts
+++ src/app/_services/document.service.ts
@@ -12,8 +12,10 @@ const httpOptions = {
 @Injectable()
 export class DocumentService {
 
-    private urlDoc = 'http://localhost:3333/showdoc'
-    private urlMandate = 'http://localhost:3333/mandate/'
+    private urlDoc = 'http://localhost:3333/showdoc';
+    private urlMandate = 'http://localhost:3333/mandate/';
+    private urlPid = 'http://localhost:3333/create/pid/';
+    private urlBusinessCase = 'http://localhost:3333/create/businesscase/';
 
     constructor(private http: HttpClient) { }
 
@@ -28,4 +30,15 @@ export class DocumentService {
         return this.http.get(fullUrl, httpOptions);
     }
 
+    public uploadPid(body, idProject, idProcess): Observable<Project> {
+        const fullUrl = this.urlPid + idProject + '/' + idProcess;
+        return this.http.post<Project>(fullUrl, body);
+
+    }
+
+    public uploadBusinessCase(body, idProject, idProcess): Observable<Project> {
+        const fullUrl = this.urlBusinessCase + idProject + '/' + idProcess;
+        return this.http.post<Project>(fullUrl, body);
+
+    }
 }
